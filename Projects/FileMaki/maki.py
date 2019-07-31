64571bd9b3e7496cf250bb8bfb0cf6b37f0ca3b2
jmbaymon <jmbaymon@github.com>
2019-07-30 19:04:43 -0500

    added files s

---
+++ Projects/FileMaki/maki.py
@@ -0,0 +1,9 @@
+import shutil , os, send2trash
+
+"""
+Maki will have the following methods:
+[ ] recycle file to trash 
+[ ] backup/move folders
+[ ] Folder/ files Display
+[ ] Create files/ folders
+"""
