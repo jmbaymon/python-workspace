a42eba5451c6ced5ee676e841a0456ea7acce5db
Jamal Baymon <jmbaymon@github.com>
2019-10-16 14:24:27 -0400

    push-10-16-2019

--- auto/fileManage/f2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for root, dirs, files in os.walk(".", topdown=False):
-   for name in files:
-       if name != ".*": 
-      print(os.path.join(root, name))
-   #for name in dirs:
-    #  print(os.path.join(root, name))
