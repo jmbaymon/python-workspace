56fa9c60b9b0211b2568d729f0ff0999055d7947
Jamal Baymon <jmbaymon@github.com>
2019-10-19 10:01:27 -0400

    v1 of pomodoro

--- workbook/100daysofPython/day1-3/pomodoro.py
+++ workbook/100daysofPython/day1-3/pomodoro.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 from datetime import timedelta
-from datetime import time
+import time
 
 
 # User selects time option 
 
-timeselection = input("Enter the amount of minutes: ")
+timeselection = input("Enter the minutes: ")
 
 def timer(timeselection):
     tmins = timedelta(minutes=timeselection)
     while tmins != timedelta( minutes=0 , seconds=0):
         print(tmins)
-        time.
+        time.sleep(1)
         tmins = tmins - timedelta(seconds=1)
+    print("Take a break.")
 
 timer(timeselection)
