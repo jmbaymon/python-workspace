d278873f67af44d4ad1a751eb542c0669d99355f
jmbaymon <jmbaymon@github.com>
2019-10-28 17:48:28 -0400

    completed ch01

---
+++ workbook/EssentialPY/Ex_Files_Python_EssT/Exercise Files/Chap02/blocks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 42
+y = 73
+
+if x < y:
+     z = 112
+     print('x < y: x is {} and y is {}'.format(x, y))
+     print('line 3')
+     print('line 3')
+     print('line 4')
+
+# if x< y:print('x < y: x is {} and y is {}'.format(x, y))
+print(f'z is {z}')
+
+
